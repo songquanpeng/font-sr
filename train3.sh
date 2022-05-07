4e37ed35eeb00302a1e8bd5f231202d63332bb0f
JustSong <songquanpeng@foxmail.com>
2022-05-07 12:35:11 +0800

    bug fix

--- train3.sh
+++ train3.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=0 python train.py \
 --dataroot ./archive/FONT_SR_32to256_ttf_10_test3_resizeFalse_anti_aliasFalse \
+--display_port 8099 \
 --name super_font_cyclegan3 --model cycle_gan
